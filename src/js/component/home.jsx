50a14aeedd6b9f7a281cc92a0d8e3bd0f37ed6b7
Mercad0 <abimaelmercado05@gmail.com>
2022-10-26 22:49:41 +0000

    made buttons smaller

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,52 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
 const Home = () => {
-	const [second,setSecond]=useState(0);
-		const [seconds,setSeconds]=useState(0);
-			const [minutes,setMinutes]=useState(0)
-														let home;
-															useEffect(()=> {
-																home= setInterval(()=> {
-																	setSecond(second+1);
+  const [second, setSecond] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  let home;
+  useEffect(() => {
+    home = setInterval(() => {
+      setSecond(second + 1);
 
-																		if(second === 59){
-																			setSeconds(seconds+1);
-																				setSecond(0);		
-																							 }
-																								}, 1000)
+      if (second === 59) {
+        setSeconds(seconds + 1);
+        setSecond(0);
+      }
+    }, 1000);
 
-																						return ()=> clearInterval(home);
-																														});
+    return () => clearInterval(home);
+  });
 
+  const restart = () => {
+    setInterval(0);
+    setSecond(0);
+    setSeconds(0);
+  };
 
-																	const restart = () => {
-																		setInterval(0)
-																		setSecond(0)
-																		setSeconds(0)
-																	}					
-																	
-																	const stop = () => {
-																		clearInterval(home)
-																	}
+  const stop = () => {
+    clearInterval(home);
+  };
 
+  return (
+    <div className="container">
+      <div className="stopWatch">
+        <div className="TimerLogo">
+          <FontAwesomeIcon icon={faClock} />
+        </div>
+        <div className="minutes"></div>
+        <div className="seconds">{seconds < 10 ? "0" + seconds : seconds}</div>
+        <div className="second">:{second < 10 ? "0" + second : second}</div>
+      </div>
 
-	return ( 
-		<div className="container">
-			<div className= "stopWatch">
-				<div className ="TimerLogo" >
-					<FontAwesomeIcon icon={faClock} />
-						</div>
-							<div className ="minutes"></div>
-								<div className ="seconds">{ seconds<10? "0"+ seconds: seconds }</div>
-									<div className ="second">:{second<10? "0"+ second: second }</div>
-										</div>
-										<button className="restart" onClick={restart}>Restart</button>
-										<button className="stop" onClick={stop}>Stop</button>
-											</div>
-		)};
-
+      <button className="restart" onClick={restart}>
+        Restart
+      </button>
+      <button className="stop" onClick={stop}>
+        Stop
+      </button>
+    </div>
+  );
+};
 
 export default Home;
